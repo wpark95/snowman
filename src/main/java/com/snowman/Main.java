cccb8bf259962e21915627071146d1e41f707110
blworth <worthambrandon@gmail.com>
2022-08-08 12:29:42 -0500

    Created instance of Reader.

--- src/main/java/com/snowman/Main.java
+++ src/main/java/com/snowman/Main.java
@@ -1,9 +1,16 @@
 package com.snowman;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class Main {
 
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws IOException {
+    Reader input =  new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+    System.out.println(reader.readLine());
   }
 
 }
