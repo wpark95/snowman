72b3455f48ac5c952e8cb6b733861197a8beba94
blworth <worthambrandon@gmail.com>
2022-08-09 15:19:42 -0500

    Finished for loop grabbing random word from .csv

--- src/main/java/com/snowman/WordList.java
+++ src/main/java/com/snowman/WordList.java
@@ -1,31 +1,26 @@
 package com.snowman;
 
-import com.sun.nio.sctp.PeerAddressChangeNotification;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class WordList {
+  public static final String WORD_LIST_FILE = "./src/main/resources/snowman-words-four-to-fifteen.csv";
 
-  public static final String WORD_LIST_FILE = "./src/main/resources/two-char-words.txt";
-//  public static final String WORD_LIST_FILE = "./src/main/resources/snowman words.csv";
-
-  public static void main(String[] args) throws FileNotFoundException {
-
+  private static String wordChoice(int userLength) throws FileNotFoundException {
     Scanner scanner = new Scanner(new File(WORD_LIST_FILE));
-    List<String> words = new ArrayList<>();
-
-    while (scanner.hasNext()) {
-      words.add(scanner.nextLine());
+    String[] words = new String[0];
+    int userChoice = (userLength - 4);
+    int randomIndex = 0;
+    for (int i = 0; i <= userChoice; i++) {
+      String currentLine = scanner.nextLine();
+      if (i == userChoice) {
+        words = currentLine.split(",");
+        randomIndex = (int) ((Math.random() * ((words.length - 1))));
+      }
     }
+    return words[randomIndex];
 
-    Random rand = new Random();
-
-    String secretWord = words.get(rand.nextInt(words.size()));
-
-    System.out.println(secretWord);
   }
+
 }
