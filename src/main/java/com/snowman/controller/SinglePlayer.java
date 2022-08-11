7261dddb8dd1b5ac96f74facb218feb0f108c5bc
blworth <worthambrandon@gmail.com>
2022-08-11 13:15:13 -0500

    Added do-while to SinglePlayer, no invalid userInputs allowed.

--- src/main/java/com/snowman/controller/SinglePlayer.java
+++ src/main/java/com/snowman/controller/SinglePlayer.java
@@ -11,45 +11,60 @@ import java.util.ResourceBundle;
 
 public class SinglePlayer {
 
-  public static final String INVALID_WORD_LENGTH =
-      "Word length must be between 4 and 15 (inclusive). You entered: %1$d. Please try again.";
-  public static final String INVALID_GUESS_NUM =
-      "Number of guesses must be between 1 and 30 (inclusive). You entered: %1$d. Please try again.";
   public static final int DEFAULT_MAX_GUESS = 30;
   public static final int DEFAULT_MIN_GUESS = 1;
   public static final int DEFAULT_MAX_WORD_LENGTH = 15;
   public static final int DEFAULT_MIN_WORD_LENGTH = 4;
+  public String userInput;
 
   private final Game game;
 
-  public SinglePlayer(BufferedReader reader, WordListProcessor words, ResourceBundle bundle, Random rng)
+  public SinglePlayer(BufferedReader reader, WordListProcessor words, ResourceBundle bundle)
       throws IOException {
     String wordLengthPreferencePrompt = bundle.getString(Keys.WORD_LENGTH_PREFERENCE);
     String guessNumPreferencePrompt = bundle.getString(Keys.GUESS_NUM_PREFERENCE);
 
-    System.out.println(wordLengthPreferencePrompt);
-    int wordLength = Integer.parseInt(reader.readLine().trim());
-    if (wordLength == 0) {
-      wordLength =
-          rng.nextInt(DEFAULT_MAX_WORD_LENGTH - DEFAULT_MIN_WORD_LENGTH) + DEFAULT_MIN_WORD_LENGTH;
-    } else {
-      if (wordLength > DEFAULT_MAX_WORD_LENGTH || wordLength < DEFAULT_MIN_WORD_LENGTH) {
-        throw new IllegalArgumentException(String.format(INVALID_WORD_LENGTH, wordLength));
+    int userWordLengthCount = 0;
+    do {
+      try {
+        userWordLengthCount = promptUserWordLength(reader, wordLengthPreferencePrompt);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      } catch (NumberFormatException e) {
+        //ignore exception
       }
-    }
+    } while (userWordLengthCount < DEFAULT_MIN_WORD_LENGTH || userWordLengthCount > DEFAULT_MAX_WORD_LENGTH);
+    int wordLength = userWordLengthCount;
 
-    System.out.println(guessNumPreferencePrompt);
-    int remainingGuess = Integer.parseInt(reader.readLine().trim());
-    if (remainingGuess == 0) {
-      remainingGuess = rng.nextInt(DEFAULT_MAX_GUESS - DEFAULT_MIN_GUESS) + DEFAULT_MIN_GUESS;
-    } else {
-      if (remainingGuess > DEFAULT_MAX_GUESS || remainingGuess < DEFAULT_MIN_GUESS) {
-        throw new IllegalArgumentException(String.format(INVALID_GUESS_NUM, remainingGuess));
+    int userGuessCount = 0;
+    do {
+      try {
+        userGuessCount = promptUserGuess(reader, guessNumPreferencePrompt);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      } catch (NumberFormatException e) {
+        //ignore exception
       }
-    }
+    } while (userGuessCount < DEFAULT_MIN_GUESS || userGuessCount > DEFAULT_MAX_GUESS);
+    int remainingGuess = userGuessCount;
 
-    game = new Game(new HashSet<>(), wordLength, remainingGuess, words.wordChoice(wordLength), reader);
+    game = new Game(new HashSet<>(), wordLength, remainingGuess, words.wordChoice(wordLength),
+        reader);
     game.play();
+  }
+
+  private int promptUserWordLength(BufferedReader reader, String prompt)
+      throws IOException, NumberFormatException {
+    System.out.println(prompt);
+    userInput = reader.readLine().trim();
+    return Integer.parseInt(userInput);
+  }
+
+  private int promptUserGuess(BufferedReader reader, String prompt)
+      throws IOException, NumberFormatException {
+    System.out.println(prompt);
+    userInput = reader.readLine().trim();
+    return Integer.parseInt(userInput);
   }
 
 }
