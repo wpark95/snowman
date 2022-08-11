2d0d507b7f435b25fba852e4ae523f8f19440283
wpark95 <park.wongyun@gmail.com>
2022-08-11 09:36:22 -0700

    Restructure the project according to the MVC model.

--- src/main/java/com/snowman/controller/SinglePlayer.java
+++ src/main/java/com/snowman/controller/SinglePlayer.java
@@ -1,77 +1,55 @@
 package com.snowman.controller;
 
-import com.snowman.model.WordProcessor;
-import com.snowman.view.SnowmanPrinter;
+import com.snowman.model.Game;
+import com.snowman.model.WordListProcessor;
+import com.snowman.view.Keys;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.Random;
+import java.util.ResourceBundle;
 
 public class SinglePlayer {
 
-  public static final int MAX_GUESS = 30;
-  public static final int MIN_GUESS = 1;
-  public static final int MAX_WORD_LENGTH = 15;
-  public static final int MIN_WORD_LENGTH = 4;
-  private static int remainingGuess;
-  private static int wordLength;
-  private static String wordPlaceholder;
-  private static String secretWord;
-  private static final Set<String> triedWords = new HashSet<>();
+  public static final String INVALID_WORD_LENGTH =
+      "Word length must be between 4 and 15 (inclusive). You entered: %1$d. Please try again.";
+  public static final String INVALID_GUESS_NUM =
+      "Number of guesses must be between 1 and 30 (inclusive). You entered: %1$d. Please try again.";
+  public static final int DEFAULT_MAX_GUESS = 30;
+  public static final int DEFAULT_MIN_GUESS = 1;
+  public static final int DEFAULT_MAX_WORD_LENGTH = 15;
+  public static final int DEFAULT_MIN_WORD_LENGTH = 4;
 
-  public void singlePlayerController(BufferedReader reader, WordProcessor words) throws IOException {
-    getUserGamePreference(reader);
-    wordPlaceholder = "_".repeat(wordLength);
-    secretWord = words.wordChoice(wordLength);
-    System.out.println(secretWord); // TODO: Delete after testing
-    while (remainingGuess >= 0) {
-      printGameState();
-      SnowmanPrinter.printSnowman(remainingGuess, wordLength);
-      System.out.println("Any guess?");
-      String userGuess = reader.readLine().toLowerCase().trim();
-      boolean wordGuessResult = WordProcessor.wordGuess(userGuess, triedWords, secretWord, wordPlaceholder);
-      if (!wordGuessResult) {
-        remainingGuess--;
-      } else {
-        wordPlaceholder = WordProcessor.changeWordPlaceholder(userGuess, secretWord, wordPlaceholder);
-      }
+  private final Game game;
 
-    }
-  }
+  public SinglePlayer(BufferedReader reader, WordListProcessor words, ResourceBundle bundle, Random rng)
+      throws IOException {
+    String wordLengthPreferencePrompt = bundle.getString(Keys.WORD_LENGTH_PREFERENCE);
+    String guessNumPreferencePrompt = bundle.getString(Keys.GUESS_NUM_PREFERENCE);
 
-  private static void printGameState() {
-    System.out.println("Already guessed: " + triedWords);
-    System.out.println("Your guess so far: " + wordPlaceholder);
-    System.out.println("Remaining guesses: " + remainingGuess);
-  }
-
-  private static void getUserGamePreference(BufferedReader reader) throws IOException {
-    System.out.println(
-        "Hey there! How many guesses would you like? This can be 1 - 30 (inclusive).");
-    String userInput = reader.readLine().trim();
-    if (userInput.length() == 0) {
-      remainingGuess = MIN_GUESS + (int) (Math.random() * (MAX_GUESS - MIN_GUESS) + 1); //TODO verify this equation, just in case ;)
+    System.out.println(wordLengthPreferencePrompt);
+    int wordLength = Integer.parseInt(reader.readLine().trim());
+    if (wordLength == 0) {
+      wordLength =
+          rng.nextInt(DEFAULT_MAX_WORD_LENGTH - DEFAULT_MIN_WORD_LENGTH) + DEFAULT_MIN_WORD_LENGTH;
     } else {
-      int userGuessNumInput = Integer.parseInt(userInput);
-      if (userGuessNumInput > MAX_GUESS || userGuessNumInput < MIN_GUESS) {
-        throw new IllegalArgumentException(
-            "Number of guesses must be a number between 1 and 30 (inclusive).");
+      if (wordLength > DEFAULT_MAX_WORD_LENGTH || wordLength < DEFAULT_MIN_WORD_LENGTH) {
+        throw new IllegalArgumentException(String.format(INVALID_WORD_LENGTH, wordLength));
       }
-      remainingGuess = userGuessNumInput;
     }
-    System.out.println("How long do you want the word to be? This can be 4 - 15 (inclusive).");
-    String userInput2 = reader.readLine().trim();
-    if (userInput2.length() == 0) {
-      wordLength = MIN_WORD_LENGTH + (int) (Math.random() * (MAX_WORD_LENGTH - MIN_WORD_LENGTH)
-          + 1);
+
+    System.out.println(guessNumPreferencePrompt);
+    int remainingGuess = Integer.parseInt(reader.readLine().trim());
+    if (remainingGuess == 0) {
+      remainingGuess = rng.nextInt(DEFAULT_MAX_GUESS - DEFAULT_MIN_GUESS) + DEFAULT_MIN_GUESS;
     } else {
-      int userWordLengthInput = Integer.parseInt(userInput2);
-      if (userWordLengthInput > MAX_WORD_LENGTH || userWordLengthInput < MIN_WORD_LENGTH) {
-        throw new IllegalArgumentException(
-            "Word length must be a number between 4 and 15 (inclusive).");
+      if (remainingGuess > DEFAULT_MAX_GUESS || remainingGuess < DEFAULT_MIN_GUESS) {
+        throw new IllegalArgumentException(String.format(INVALID_GUESS_NUM, remainingGuess));
       }
-      wordLength = userWordLengthInput;
     }
+
+    game = new Game(new HashSet<>(), wordLength, remainingGuess, words.wordChoice(wordLength), reader);
+    game.play();
   }
 
 }
--- src/main/java/com/snowman/view/MessagePrinter.java
+++ src/main/java/com/snowman/view/MessagePrinter.java
@@ -2,67 +2,67 @@ package com.snowman.view;
 
 import com.snowman.Main;
 import java.io.IOException;
-import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
 
-public class SnowmanPrinter {
+public class MessagePrinter {
 
-  private static final String hatTop =         "  *      /*\\  *    *";
+  private static final String hatTop = "  *      /*\\  *    *";
 
-  private static final String hatLow =         "    *  _/ _ \\_ *   * ";
+  private static final String hatLow = "    *  _/ _ \\_ *   * ";
 
-  private static final String neutralFace =    " *   * ('< ' ) *      *";
+  private static final String neutralFace = " *   * ('< ' ) *      *";
 
-  private static final String sadFace =        " *   * (T< T ) *      *";
+  private static final String sadFace = " *   * (T< T ) *      *";
 
-  private static final String happyFace =      " *   * (^< ^ ) *      *";
+  private static final String happyFace = " *   * (^< ^ ) *      *";
 
-  private static final String bodyTop =        "   * \\–(  :   )–/   *";
+  private static final String bodyTop = "   * \\–(  :   )–/   *";
 
-  private static final String bodyLow =        "   *   (__: __)  *    *";
+  private static final String bodyLow = "   *   (__: __)  *    *";
 
   public static void printSnowman(int remainingGuess) throws IOException {
 
     switch (remainingGuess) {
       case 0:
-        SnowmanPrinter.youLoseSnowman();
+        MessagePrinter.printLoseMessage();
         Main.main(null);
         break;
       case 1:
-        SnowmanPrinter.halfSnowman();
+        MessagePrinter.printHalfSnowman();
         break;
       case 2:
-        SnowmanPrinter.withoutLowerBodySnowman();
+        MessagePrinter.printTopBodyOnlySnowman();
         break;
       case 3:
-        SnowmanPrinter.neutralSnowman();
+        MessagePrinter.printNeutralSnowman();
         break;
       default:
-        SnowmanPrinter.happySnowman();
+        MessagePrinter.printHappySnowman();
         break;
     }
-
   }
 
-  private static void happySnowman() {
+  private static void printHappySnowman() {
     hat();
     System.out.println(happyFace);
     System.out.println(bodyTop); // TODO: Fix redundancy in printing the snowman.
     System.out.println(bodyLow);
   }
 
-  private static void neutralSnowman() {
+  private static void printNeutralSnowman() {
     hat();
     System.out.println(neutralFace);
     System.out.println(bodyTop); // TODO: Fix redundancy in printing the snowman.
     System.out.println(bodyLow);
   }
 
-  private static void withoutLowerBodySnowman() {
-    halfSnowman();
+  private static void printTopBodyOnlySnowman() {
+    printHalfSnowman();
     System.out.println(bodyTop);
   }
 
-  private static void halfSnowman() {
+  private static void printHalfSnowman() {
     hat();
     System.out.println(sadFace);
   }
@@ -72,7 +72,7 @@ public class SnowmanPrinter {
     System.out.println(hatLow);
   }
 
-  public static void youWinSnowman() {
+  public static void printWinMessage() {
     System.out.println("\n"
         + "░█████╗░░█████╗░███╗░░██╗░██████╗░██████╗░░█████╗░████████╗██╗░░░██╗██╗░░░░░░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗\n"
         + "██╔══██╗██╔══██╗████╗░██║██╔════╝░██╔══██╗██╔══██╗╚══██╔══╝██║░░░██║██║░░░░░██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝\n"
@@ -88,13 +88,20 @@ public class SnowmanPrinter {
         + "░░░╚═╝░░░░╚════╝░░╚═════╝░  ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝");
   }
 
-  public static void youLoseSnowman() {
+  public static void printLoseMessage() {
     System.out.println("\n" + "██╗░░░██╗░█████╗░██╗░░░██╗  ██╗░░░░░░█████╗░░██████╗███████╗\n"
         + "╚██╗░██╔╝██╔══██╗██║░░░██║  ██║░░░░░██╔══██╗██╔════╝██╔════╝\n"
         + "░╚████╔╝░██║░░██║██║░░░██║  ██║░░░░░██║░░██║╚█████╗░█████╗░░\n"
         + "░░╚██╔╝░░██║░░██║██║░░░██║  ██║░░░░░██║░░██║░╚═══██╗██╔══╝░░\n"
         + "░░░██║░░░╚█████╔╝╚██████╔╝  ███████╗╚█████╔╝██████╔╝███████╗\n"
         + "░░░╚═╝░░░░╚════╝░░╚═════╝░  ╚══════╝░╚════╝░╚═════╝░╚══════╝");
+  }
+
+  public static void printCurrentState(int remainingGuess, String wordPlaceholder,
+      Set<String> triedWords) {
+    System.out.println("Your remaining guesses: " + remainingGuess);
+    System.out.println("You have tried so far: " + triedWords);
+    System.out.println("Mystery word : " + wordPlaceholder);
   }
 
 }
