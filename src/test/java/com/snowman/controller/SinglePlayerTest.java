c36974c2f271cbe741496392ed38a49956e8f9ca
wpark95 <park.wongyun@gmail.com>
2022-08-12 17:52:10 -0700

    Clean up SinglePlayerTest

--- src/test/java/com/snowman/controller/SinglePlayerTest.java
+++ src/test/java/com/snowman/controller/SinglePlayerTest.java
@@ -6,9 +6,22 @@ import com.snowman.model.Game;
 import com.snowman.model.WordListProcessor;
 import com.snowman.view.SinglePlayerView;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.nio.CharBuffer;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SinglePlayerTest {
 
@@ -16,19 +29,17 @@ class SinglePlayerTest {
   private final SinglePlayerView view = new SinglePlayerView();
   private final WordListProcessor words = new WordListProcessor();
   private final ResourceBundle bundle = ResourceBundle.getBundle("strings");
-  private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+  private final String mockInput = "alpha";
+  ByteArrayInputStream mockInputStream = new ByteArrayInputStream(mockInput.getBytes());
+  private BufferedReader reader = new BufferedReader(new InputStreamReader(mockInputStream));
   private final SinglePlayer controller = new SinglePlayer(game, view, words, bundle, reader);
 
   @Test
-  void play() {
-  }
-
-  @Test
-  void getInitialSetupTest() {
-  }
-
-  @Test
-  void getUserGuess() {
+  void testGetInitialSetup_valid() throws IOException {
+    //TODO: Update testGetInitialSetup_valid
+    String actualInput = controller.getUserGuess();
+    reader.readLine();
+    assertEquals(mockInput, actualInput);
   }
 
 }
